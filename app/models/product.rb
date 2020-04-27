047b41b95749c3e94f429659caa2298216a4a862
jose.saldana <josaldmer@gmail.com>
2020-04-27 19:27:06 +0200

    Added category id to products json

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  validates :name, :price, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   belongs_to :category
   validates :category, presence: true
+  validates :price, presence: true
 
   scope :by_category, ->(category_id) { where(category_id: category_id) }
 
--- app/views/api/v1/products/_product.json.jbuilder
+++ app/views/api/v1/products/_product.json.jbuilder
@@ -4,3 +4,4 @@ json.id product.id
 json.name product.name
 json.description product.description
 json.price product.price
+json.category_id product.category.id
